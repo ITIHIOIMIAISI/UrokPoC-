a4b4446f368dab0391fb6c862dd7e526d2897185
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-03 23:52:28 +0300

    005

---
+++ Example005/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Ведите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "артём")
+{
+    Console.WriteLine("Братааааан, давно невиделись");
+    Console.WriteLine("Как дела ?");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}

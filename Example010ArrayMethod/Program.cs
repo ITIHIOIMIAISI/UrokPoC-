7f55481ac29a7c5e0dbdbd26de097cf44843457f
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-08 22:12:03 +0300

    Переписал с лекции  010

--- Example010ArrayMethod/Program.cs
+++ Example010ArrayMethod/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] array = {12, 345, 21, 76, 123, 75, 34, 12, 523, 568, 9634, 34, 456};
+
+int n = array.Length;
+int find = 34;
+
+int index = 0;
+
+while (index < n)
+{
+    if (array[index] == find)
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}

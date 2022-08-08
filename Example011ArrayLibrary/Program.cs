5e7339caac62e7a4d6067dc30199ee40cd5f214c
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-09 00:11:21 +0300

    Переписал с лекции 011

--- Example011ArrayLibrary/Program.cs
+++ Example011ArrayLibrary/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿void FillArray(int[] collection)
+{
+    int lingth = collection.Length;
+    int index = 0;
+    while (index < lingth)
+    {
+        collection[index] = new Random().Next(1, 10);
+        index++;
+    }
+}
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int Index0f(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if (collection[index] == find)
+        {
+            position = index;
+        }
+        index++;
+    }
+    return position;
+}
+
+int[] array = new int[12]; //Создай массив в котором будет 10 элементов
+
+FillArray(array);
+PrintArray(array);
+Console.WriteLine();
+
+int pos = Index0f(array, 5);
+Console.WriteLine(pos);

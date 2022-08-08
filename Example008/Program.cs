1802cbf233c8c283448a0d09ec98e787a82a23e0
ITIHIOIMIAISI <kotomacko88@gmail.com>
2022-08-08 21:58:09 +0300

    Переписал

--- Example008/Program.cs
+++ Example008/Program.cs
@@ -1,1 +1,25 @@
-﻿
+﻿int Max(int arg1, int arg2, int arg3)
+{
+int result = arg1;
+if (arg2 > result) result = arg2;
+if (arg3 > result) result = arg3;
+return result;
+}
+int a1 = 354;
+int b1 = 73;
+int c1 = 673;
+int a2 = 4;
+int b2 = 24;
+int c2 = 87;
+int a3 = 83;
+int b3 = 58;
+int c3 = 4272;
+
+/* int max1 = Max(a1, b1, c1);
+int max2 = Max(a2, b2, c2);
+int max3 = Max(a3, b3, c3);
+int max = Max(max1, max2, max3); */
+
+int max = Max(Max(a1, b1, c1), Max(a2, b2, c2), Max(a3, b3, c3));
+
+Console.WriteLine(max);
